08cd5957c57d49a1fe609efdd7d7be376f70c818
bensan <bensan.george@gmail.com>
2018-01-07 18:03:54 -0500

    Updating script for new PDO

--- migrate.php
+++ migrate.php
@@ -25,14 +25,15 @@
  *  The rest is in the usage report.
  */
 if (count($argv) <= 1) {
-  echo "Usage:
+    echo "Usage:
      To add new migration:
          php php-mysql-migrate/migrate.php add <name>
      To migrate your database:
          php php-mysql-migrate/migrate.php migrate [--skip-errors]
      ";
-  exit;
+    exit;
 }
+
 
 require_once('config.php');
 @define('MIGRATE_VERSION_FILE', '.version');
@@ -41,27 +42,28 @@ require_once('config.php');
 @define('DEBUG', false);
 
 if (count($argv) <= 1) {
-  echo "See readme file for usage.\n";
-  exit;
+    echo "See readme file for usage.\n";
+    exit;
 }
 
 // Connect to the database.
 if (!@DEBUG) {
-  $link = mysql_connect(DBADDRESS, DBUSERNAME, DBPASSWORD);
-  if (!$link) {
-    echo "Failed to connect to the database.\n";
-    exit;
-  }
-  mysql_select_db(DBNAME, $link);
-  mysql_query("SET NAMES 'utf8'", $link);
+//  $link = mysql_connect(DBADDRESS, DBUSERNAME, DBPASSWORD);
+    $link = new PDO("mysql:host=".DBADDRESS.';dbname='.DBNAME,
+        DBUSERNAME, DBPASSWORD);
+    if (!$link) {
+        echo "Failed to connect to the database.\n";
+        exit;
+    }
+    $link->query("SET NAMES 'utf8'");
 }
 
 // Find the latest version or start at 0.
 $version = 0;
 $f = @fopen(MIGRATE_VERSION_FILE, 'r');
 if ($f) {
-  $version = intval(fgets($f));
-  fclose($f);
+    $version = intval(fgets($f));
+    fclose($f);
 }
 echo "Current database version is: $version\n";
 
@@ -70,143 +72,144 @@ global $skip_errors;
 $skip_errors = false;
 
 function query($query) {
-  global $link;
-  global $skip_errors;
+    global $link;
+    global $skip_errors;
 
-  if (@DEBUG) {
-    return true;
-  }
-
-  echo "Query: $query\n";
-
-  $result = mysql_query($query, $link);
-  if (!$result) {
-    if ($skip_errors) {
-      echo "Query failed: " . mysql_error($link) . "\n";
+    if (@DEBUG) {
+        return true;
     }
-    else {
-      echo "Migration failed: " . mysql_error($link) . "\n";
-      echo "Aborting.\n";
-      mysql_query('ROLLBACK', $link);
-      mysql_close($link);
-      exit;
+
+    echo "Query: $query\n";
+
+    $result = $link->query($query);
+    if (!$result) {
+        if ($skip_errors) {
+            echo "Query failed: " . $link->errorInfo() . "\n";
+        }
+        else {
+            echo "Migration failed: " . $link->errorInfo() . "\n";
+            echo "Aborting.\n";
+            $link->query('ROLLBACK');
+            $result->closeCursor();
+            $link = null;
+            exit;
+        }
     }
-  }
-  return $result;
+    return $result;
 }
 
 function get_migrations() {
-  // Find all the migration files in the directory and return the sorted.
-  $files = array();
-  $dir = opendir(MIGRATIONS_DIR);
-  while ($file = readdir($dir)) {
-    if (substr($file, 0, strlen(MIGRATE_FILE_PREFIX)) == MIGRATE_FILE_PREFIX) {
-      $files[] = $file;
+    // Find all the migration files in the directory and return the sorted.
+    $files = array();
+    $dir = opendir(MIGRATIONS_DIR);
+    while ($file = readdir($dir)) {
+        if (substr($file, 0, strlen(MIGRATE_FILE_PREFIX)) == MIGRATE_FILE_PREFIX) {
+            $files[] = $file;
+        }
     }
-  }
-  asort($files);
-  return $files;
+    asort($files);
+    return $files;
 }
 
 function get_version_from_file($file) {
-  return intval(substr($file, strlen(MIGRATE_FILE_PREFIX)));
+    return intval(substr($file, strlen(MIGRATE_FILE_PREFIX)));
 }
 
 if ($argv[1] == 'add') {
-  $new_version = $version;
+    $new_version = $version;
 
-  // Check the new version against existing migrations.
-  $files = get_migrations();
-  $last_file = end($files);
-  if ($last_file !== false) {
-    $file_version = get_version_from_file($last_file);
-    if ($file_version > $new_version)
-      $new_version = $file_version;
-  }
-
-  // Create migration file path.
-  $new_version++;
-  $path = MIGRATIONS_DIR . MIGRATE_FILE_PREFIX . sprintf('%04d', $new_version);
-  if (@strlen($argv[2])) {
-    $path .= '-' . str_replace(' ', '-', $argv[2]);
-  }
-  $path .= MIGRATE_FILE_POSTFIX;
-
-  echo "Adding a new migration script: $path\n";
-
-  $f = @fopen($path, 'w');
-  if ($f) {
-    fputs($f, "<?php\n\nquery(\$query);\n\n");
-    fclose($f);
-    echo "Done.\n";
-  }
-  else {
-    echo "Failed.\n";
-  }
-}
-else if ($argv[1] == 'migrate') {
-  $files = get_migrations();
-
-  $skip_errors = @$argv[2] == '--skip-errors';
-
-  // Check to make sure there are no conflicts such as 2 files under the same version.
-  $errors = array();
-  $last_file = false;
-  $last_version = false;
-  foreach ($files as $file) {
-    $file_version = get_version_from_file($file);
-    if ($last_version !== false && $last_version === $file_version) {
-      $errors[] = "$last_file --- $file";
-    }
-    $last_version = $file_version;
-    $last_file = $file;
-  }
-  if (count($errors) > 0) {
-    echo "Error: You have multiple files using the same version. " .
-      "To resolve, move some of the files up so each one gets a unique version.\n";
-    foreach ($errors as $error) {
-      echo "  $error\n";
-    }
-    exit;
-  }
-
-  // Run all the new files.
-  $found_new = false;
-  foreach ($files as $file) {
-    $file_version = get_version_from_file($file);
-    if ($file_version <= $version) {
-      continue;
+    // Check the new version against existing migrations.
+    $files = get_migrations();
+    $last_file = end($files);
+    if ($last_file !== false) {
+        $file_version = get_version_from_file($last_file);
+        if ($file_version > $new_version)
+            $new_version = $file_version;
     }
 
-    echo "Running: $file\n";
-    query('BEGIN');
-    include(MIGRATIONS_DIR . $file);
-    query('COMMIT');
-    echo "Done.\n";
+    // Create migration file path.
+    $new_version++;
+    $path = MIGRATIONS_DIR . MIGRATE_FILE_PREFIX . sprintf('%04d', $new_version);
+    if (@strlen($argv[2])) {
+        $path .= '-' . str_replace(' ', '-', $argv[2]);
+    }
+    $path .= MIGRATE_FILE_POSTFIX;
 
-    $version = $file_version;
-    $found_new = true;
+    echo "Adding a new migration script: $path\n";
 
-    // Output the new version number.
-    $f = @fopen(MIGRATE_VERSION_FILE, 'w');
+    $f = @fopen($path, 'w');
     if ($f) {
-      fputs($f, $version);
-      fclose($f);
+        fputs($f, "<?php\n\nquery(\$query);\n\n");
+        fclose($f);
+        echo "Done.\n";
     }
     else {
-      echo "Failed to output new version to " . MIGRATION_VERSION_FILE . "\n";
+        echo "Failed.\n";
     }
-  }
+}
+else if ($argv[1] == 'migrate') {
+    $files = get_migrations();
 
-  if ($found_new) {
-    echo "Migration complete.\n";
-  }
-  else {
-    echo "Your database is up-to-date.\n";
-  }
+    $skip_errors = @$argv[2] == '--skip-errors';
+
+    // Check to make sure there are no conflicts such as 2 files under the same version.
+    $errors = array();
+    $last_file = false;
+    $last_version = false;
+    foreach ($files as $file) {
+        $file_version = get_version_from_file($file);
+        if ($last_version !== false && $last_version === $file_version) {
+            $errors[] = "$last_file --- $file";
+        }
+        $last_version = $file_version;
+        $last_file = $file;
+    }
+    if (count($errors) > 0) {
+        echo "Error: You have multiple files using the same version. " .
+            "To resolve, move some of the files up so each one gets a unique version.\n";
+        foreach ($errors as $error) {
+            echo "  $error\n";
+        }
+        exit;
+    }
+
+    // Run all the new files.
+    $found_new = false;
+    foreach ($files as $file) {
+        $file_version = get_version_from_file($file);
+        if ($file_version <= $version) {
+            continue;
+        }
+
+        echo "Running: $file\n";
+        query('BEGIN');
+        include(MIGRATIONS_DIR . $file);
+        query('COMMIT');
+        echo "Done.\n";
+
+        $version = $file_version;
+        $found_new = true;
+
+        // Output the new version number.
+        $f = @fopen(MIGRATE_VERSION_FILE, 'w');
+        if ($f) {
+            fputs($f, $version);
+            fclose($f);
+        }
+        else {
+            echo "Failed to output new version to " . MIGRATION_VERSION_FILE . "\n";
+        }
+    }
+
+    if ($found_new) {
+        echo "Migration complete.\n";
+    }
+    else {
+        echo "Your database is up-to-date.\n";
+    }
 }
 
 if (!@DEBUG) {
-  mysql_close($link);
+    $link = null;
 }
 
